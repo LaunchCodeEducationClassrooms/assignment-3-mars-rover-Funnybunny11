e74b7d65b1e48a349cc924fcb0af330f7f42f66e
Jennifer Whalen <jennwhalen7@gmail.com>
2021-03-22 02:31:53 +0000

    I wrote tests for the Rover.

--- rover.js
+++ rover.js
@@ -1,5 +1,36 @@
 class Rover {
-   // Write code here!
+   constructor(position, mode='Normal', generatorWatts=110){
+     this.position = position;
+     if(!position){
+       throw Error("Position number is required.");
+     }
+     this.mode = mode;
+     this.generatorWatts = generatorWatts
+     this.message = message;
+     if(!message){
+       throw Error('Message name required.');
+     }
+   }
+   receivesMessage(message){ 
+     
+     let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+    message = new Message('Test message with two commands', commands);
+  let rover = new Rover(98382);    
+  let response = rover.receiveMessage(message);
+
+
+console.log(response)
+console.log('STATUS_CHECK')    
+console.log('MODE_CHANGE')
+console.log('LOW_POWER', position)
+console.log('Move', Rover.position)     
+     }
+    
+     
+    
+    
 }
+
+
 
 module.exports = Rover;
--- spec/rover.spec.js
+++ spec/rover.spec.js
@@ -8,6 +8,61 @@ const Command = require('../command.js');
 
 describe("Rover class", function() {
 
-  // 7 tests here!
+  it("constructor sets position and default values for mode and generatorWatts", 
+  function () { 
+    expect (function() { new Rover();}).toThrow(new Error('Position number is required.'));
+  }); 
 
+//position is a number representing the rover's position.
+//Sets this.position to position
+//Sets this.mode to 'NORMAL'
+//Sets default value for generatorWatts to 110
+
+
+  
+  //7 tests here!
+
+
+  it("response returned by receivesMessage contains name of message", function () {
+    expect (function(){ new Message();}).toThrow(new Error('Message name required.'));
+  });
+  
+
+  it("response returned by receivesMessage includes two results if two commands are sent in the message", function (){
+    expect (function() {new Message();})
+    
+      let response = [true, true]
+      expect(response).toEqual[true,true];
+   });
+     
+  
+
+
+  it("responds correctly to status check command",function (){
+    expect (function() {new Message('STATUS_CHECK');})
+    let response = 'STATUS_CHECK'
+     expect (response).toEqual('STATUS_CHECK')
+  });
+
+
+
+it("responds correctly to mode change command.", function (){
+  expect (function() {new Message ('MODE_CHANGE');})
+  let response = 'MODE_CHANGE'
+  expect (response).toEqual('MODE_CHANGE')
+});
+
+
+it("responds with false completed value when attempting to move in LOW_POWER mode.", function (){
+  
+    expect (function() {new Message ();})
+    if (Rover.mode === 'LOW_POWER'){
+    expect (Rover.move).toEqual(98382)
+    }
+});
+
+
+it("responds with position for move command", function() {expect (function () {new Message ('MOVE');})
+expect (Rover.move).toEqual(Rover.position)
+});
 });
